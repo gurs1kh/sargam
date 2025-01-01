cad509ac06719081d5b96b6e703ceb67d8a54682
Manvir Singh <gurs1kh@live.com>
2024-12-31 16:44:41 -0800

    replace toggle button group lib with own component

---
+++ src/components/ToggleButtonGroup.tsx
@@ -0,0 +1,43 @@
+import './ToggleButtonGroup.css'
+
+interface ToggleButtonGroupProps {
+  options: { value: string, text: string }[];
+  selected: string;
+  onChange: (value: string) => void;
+}
+
+export const ToggleButtonGroup = ({ selected, options, onChange }: ToggleButtonGroupProps) => {
+  return (
+    <div className="toggle-button-group">
+      {options.map(({ value, text }) => (
+        <ToggleButton
+          key={value}
+          text={text}
+          value={value}
+          onClick={() => onChange(value)}
+          selected={value === selected}
+        />
+      ))}
+    </div>
+  );
+}
+
+interface ToggleButtonProps {
+  text: string;
+  onClick: () => void;
+  selected?: boolean;
+  value?: string;
+}
+
+export const ToggleButton = ({ text, onClick, selected, value }: ToggleButtonProps) => {
+  return (
+    <button
+      className={`toggle-button ${selected ? 'toggle-button-selected' : ''}`}
+      value={value}
+      onClick={onClick}
+    >
+      <span className="toggle-button-text">{text}</span>
+    </button>
+  );
+}
+
